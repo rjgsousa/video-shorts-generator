a012689235242589eb40c4e03b55555a077a378b
Ricardo G. Sousa <ricardo@rsousa.co>
2024-04-16 16:39:17 +0100

    feat: web scraper extracts more content from web and is more responsible when pulling the data

--- vsg-models/vsg-themes/vsg_themes/extract/from_web.py
+++ vsg-models/vsg-themes/vsg_themes/extract/from_web.py
@@ -1,28 +1,44 @@
+import logging
+
 import aiohttp
 import json
 import re
 import asyncio
 from bs4 import BeautifulSoup
 
+from vsg_utils.fs import check_dir_exists_create_if_not
+
 
 def clean_content(content):
-    # Remove links (URLs)
-    content = re.sub(r'http\S+|www.\S+', '', content, flags=re.MULTILINE)
+    pattern = re.compile(r'<a\b[^>]*>(.*?)</a>')
+
+    content = pattern.sub(r'\1', content)
 
     # Remove other irrelevant data (e.g., HTML tags, scripts, etc.) using BeautifulSoup
     soup = BeautifulSoup(content, 'html5lib')
     for script in soup(['script', 'style']):
         script.extract()
 
-    return soup.get_text().strip()
+    data = soup.get_text().strip()
+
+    return data
 
 
 async def get_data_from_url_as_json(url: str):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
 
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.text()
-            return data
+        async with session.get(url, headers=headers) as response:
+            if response.status == 200:
+                await asyncio.sleep(2)
+                data = await response.text()
+            else:
+                logging.warning(f"Response code: {response.status}")
+                data = []
+
+    return data
 
 
 async def get_data_from_url_and_save_to_file(url, output_file):
@@ -47,25 +63,12 @@ async def get_data_from_url_and_save_to_file(url, output_file):
 
 
 def get_data_from_url(url, output_file):
+
+    check_dir_exists_create_if_not(output_file)
+
     asyncio.run(
         get_data_from_url_and_save_to_file(
             url,
             output_file
         )
     )
-
-
-def load_json_data(output_file: str) -> dict:
-    data = dict()
-
-    try:
-        with open(output_file) as file:
-            data = json.load(file)
-
-    except FileNotFoundError:
-        print(f"File {output_file} not found.")
-
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON from {output_file}.")
-
-    return data
